b99d9bcc40ae2556793cf5ef5929a81674b76af1
Kim-dami <s3327550@gmail.com>
2017-02-13 17:59:08 +0900

    170213 일해요 테스트 직후 업데이트

---
+++ js/uiSlider.php
@@ -0,0 +1,338 @@
+<div id="touchSlider"> 
+ 	<ul> 
+ 		<li> 
+ 			content 1 
+ 		</li> 
+ 		<li style="background:#396"> 
+ 			content 2 
+ 		</li> 
+ 		<li style="background:#39C"> 
+ 			content 3 
+ 		</li> 
+ 		<li style="background:#33C"> 
+ 			content 4 
+ 		</li> 
+ 	</ul> 
+</div> 
+<div class="btn_area"> 
+ 	<button type="button" class="btn_prev">prev</button> 
+ 	<button type="button" class="btn_next">next</button> 
+ 	<div id="count" style="height:36px; font-size:16px; line-height:36px; text-align:center;"> 
+ 	</div> 
+</div> 
+ 
+<h2>2 : limit, page:2, speed:300</h2> 
+<div id="touchSlider2"> 
+ 	<ul> 
+ 		<li> 
+ 			content 1 
+ 		</li> 
+ 		<li style="background:#396"> 
+ 			content 2 
+ 		</li> 
+ 		<li style="background:#39C"> 
+ 			content 3 
+ 		</li> 
+ 		<li style="background:#33C"> 
+ 			content 4 
+ 		</li> 
+ 	</ul> 
+</div> 
+<div class="btn_area"> 
+ 	<button type="button" class="btn_prev">prev</button> 
+ 	<button type="button" class="btn_next">next</button> 
+</div> 
+ 
+<h2>3 : multiple, view:3, limit</h2> 
+<div id="touchSlider3"> 
+	<ul> 
+		<li> 
+			content 1 
+		</li> 
+		<li style="background:#396"> 
+			content 2 
+		</li> 
+		<li style="background:#39C"> 
+			content 3 
+		</li> 
+		<li> 
+			content 4 
+		</li> 
+		<li style="background:#396"> 
+			content 5 
+		</li> 
+		<li style="background:#39C"> 
+			content 6 
+		</li> 
+		<li> 
+			content 7 
+		</li> 
+		<li style="background:#396"> 
+			content 8 
+		</li> 
+		<li style="background:#39C"> 
+			content 9 
+		</li> 
+	</ul> 
+</div> 
+<div class="btn_area"> 
+	<button type="button" class="btn_prev">prev</button> 
+	<button type="button" class="btn_next">next</button> 
+</div> 
+ 
+<h2>4 : multiple, view:3, rolling</h2> 
+<div id="touchSlider4"> 
+	<ul> 
+		<li> 
+			content 1 
+		</li> 
+		<li style="background:#396"> 
+			content 2 
+		</li> 
+		<li style="background:#39C"> 
+			content 3 
+		</li> 
+		<li> 
+			content 4 
+		</li> 
+		<li style="background:#396"> 
+			content 5 
+		</li> 
+		<li style="background:#39C"> 
+			content 6 
+		</li> 
+		<li> 
+			content 7 
+		</li> 
+		<li style="background:#396"> 
+			content 8 
+		</li> 
+		<li style="background:#39C"> 
+			content 9 
+		</li> 
+	</ul> 
+</div> 
+<div class="btn_area"> 
+	<button type="button" class="btn_prev">prev</button> 
+	<button type="button" class="btn_next">next</button> 
+</div> 
+ 
+<h2>5 : multiple, view:5, flexible, rolling</h2> 
+<div id="touchSlider5"> 
+	<ul> 
+		<li> 
+			content 1 
+		</li> 
+		<li style="background:#396"> 
+			content 2 
+		</li> 
+		<li style="background:#39C"> 
+			content 3 
+		</li> 
+		<li> 
+			content 4 
+		</li> 
+		<li style="background:#396"> 
+			content 5 
+		</li> 
+		<li> 
+			content 6 
+		</li> 
+		<li style="background:#396"> 
+			content 7 
+		</li> 
+		<li style="background:#39C"> 
+			content 8 
+		</li> 
+		<li> 
+			content 9 
+		</li> 
+		<li style="background:#396"> 
+			content 10 
+		</li> 
+	</ul> 
+</div> 
+<div class="btn_area"> 
+	<button type="button" class="btn_prev">prev</button> 
+	<button type="button" class="btn_next">next</button> 
+</div> 
+
+<h2>6 : flexible, rolling, paging, counter</h2> 
+<div id="touchSlider6"> 
+	<ul> 
+		<li> 
+			content 1 
+		</li> 
+		<li style="background:#396"> 
+			content 2 
+		</li> 
+		<li style="background:#39C"> 
+			content 3 
+		</li> 
+		<li style="background:#33C"> 
+			content 4 
+		</li> 
+	</ul> 
+</div> 
+<div class="paging"></div> 
+
+<h2>7 : rolling, flexible, resize(default)</h2> 
+<div id="touchSlider7"> 
+	<ul> 
+		<li> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+			content 1<br /> 
+		</li> 
+		<li style="background:#396"> 
+			content 2<br /> 
+			content 2<br /> 
+			content 2<br /> 
+			content 2<br /> 
+			content 2<br /> 
+		</li> 
+		<li style="background:#39C"> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+			content 3<br /> 
+		</li> 
+		<li style="background:#33C"> 
+			content 4<br /> 
+			content 4<br /> 
+			content 4<br /> 
+			content 4<br /> 
+			content 4<br /> 
+			content 4<br /> 
+			content 4<br /> 
+		</li> 
+	</ul> 
+</div> 
+<div class="btn_area"> 
+	<button type="button" class="btn_prev">prev</button> 
+	<button type="button" class="btn_next">next</button> 
+</div> 
+
+<h2>8 : limit, flexible, right-margin</h2> 
+<div class="touchSlider8_wrap"> 
+	<div id="touchSlider8"> 
+		<ul> 
+			<li> 
+				content 1 
+			</li> 
+			<li style="background:#396"> 
+				content 2 
+			</li> 
+			<li style="background:#39C"> 
+				content 3 
+			</li> 
+			<li style="background:#33C"> 
+				content 4 
+			</li> 
+		</ul> 
+	</div> 
+</div> 
+<div class="btn_area"> 
+	<button type="button" class="btn_prev">prev</button> 
+	<button type="button" class="btn_next">next</button> 
+</div> 
+
+<h2>9 : roll, flexible, right-margin</h2> 
+<div class="touchSlider9_wrap"> 
+	<div id="touchSlider9"> 
+		<ul> 
+			<li> 
+				content 1 
+			</li> 
+			<li style="background:#396"> 
+				content 2 
+			</li> 
+			<li style="background:#39C"> 
+				content 3 
+			</li> 
+			<li> 
+				content 4 
+			</li> 
+			<li style="background:#396"> 
+				content 5 
+			</li> 
+			<li> 
+				content 6 
+			</li> 
+			<li style="background:#396"> 
+				content 7 
+			</li> 
+			<li style="background:#39C"> 
+				content 8 
+			</li> 
+			<li> 
+				content 9 
+			</li> 
+			<li style="background:#396"> 
+				content 10 
+			</li> 
+		</ul> 
+	</div> 
+</div> 
+<div class="btn_area"> 
+	<button type="button" class="btn_prev">prev</button> 
+	<button type="button" class="btn_next">next</button> 
+</div> 
+
+<h2>10: rolling, flexible, side-margin</h2> 
+<div class="touchSlider10_wrap"> 
+	<div id="touchSlider10"> 
+		<ul> 
+			<li> 
+				content 1 
+			</li> 
+			<li style="background:#396"> 
+				content 2 
+			</li> 
+			<li style="background:#39C"> 
+				content 3 
+			</li> 
+			<li> 
+				content 4 
+			</li> 
+			<li style="background:#396"> 
+				content 5 
+			</li> 
+			<li> 
+				content 6 
+			</li> 
+			<li style="background:#396"> 
+				content 7 
+			</li> 
+			<li style="background:#39C"> 
+				content 8 
+			</li> 
+			<li> 
+				content 9 
+			</li> 
+			<li style="background:#396"> 
+				content 10 
+			</li> 
+		</ul> 
+	</div> 
+</div> 
+<div class="btn_area"> 
+	<button type="button" class="btn_prev">prev</button> 
+	<button type="button" class="btn_next">next</button> 
+</div> 
